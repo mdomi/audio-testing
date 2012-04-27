695ef9f8b1e7d2455f51cbfd7f47a692d750a768
Mike Dominice <mike@mikedominice.com>
2012-04-26 23:24:00 -0400

    initial commit of some boilerplate, including node static file server

---
+++ node-static-server.js
@@ -0,0 +1,25 @@
+var static = require('node-static');
+var port = parseInt(process.argv[2], 10);
+var fileServer = new static.Server('.');
+
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        fileServer.serve(request, response, function (err, result) {
+            if (err) { // There was an error serving the file
+				console.log('HTTP/' + request.httpVersion + ' ' 
+					+ request.method + ' ' + request.url + ' ' 
+					+ err.status);
+                // Respond to the client
+				if (err.status === 404) {
+					fileServer.serveFile('./404.html', 404, {}, request, response);
+				}
+                //response.writeHead(err.status, err.headers);
+                //response.end();
+            } else {
+				console.log('HTTP/' + request.httpVersion + ' ' 
+					+ request.method + ' ' + request.url + ' ' 
+					+ response.statusCode);
+			}
+        });
+    });
+}).listen(port);
